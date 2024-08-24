c0511fdc2980ca1952cf9d7ffd4b49ac84acc036
Simon Schödler <simon.schoedler@hotmail.ch>
2024-08-24 14:34:35 +0200

    Abstracts away game entity

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@
   document.addEventListener("DOMContentLoaded", (_) => {
     const sketch = document.querySelector(".sketch") as HTMLDivElement;
     const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
 
     canvas.width = 800;
     canvas.height = 600;
 
     sketch.appendChild(canvas);
 
-    // Create a moveable player object (Arrow keys)
-    const player = new Player(canvas);
+    const player = new Player(50, 50);
+    const ball = new Ball(canvas.width / 2, canvas.height / 2);
+
+    const entities = [player, ball];
 
     document.addEventListener("keydown", (event) => {
       player.handleKeyDown(event);
@@ -17,8 +20,11 @@
 
     // Game loop using requestAnimationFrame
     function gameLoop() {
-      player.draw();
-      player.update();
+      entities.forEach((entity) => {
+        entity.update(canvas, entities);
+        entity.draw(context, canvas);
+      });
+
       requestAnimationFrame(gameLoop);
     }
 
@@ -26,39 +32,77 @@
   });
 })();
 
-class Player {
-  private size: number = 50;
-  private x: number;
-  private y: number;
+abstract class Entity {
+  protected x: number;
+  protected y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  abstract draw(
+    context: CanvasRenderingContext2D,
+    canvas: HTMLCanvasElement
+  ): void;
+  abstract update(canvas: HTMLCanvasElement, entities: Entity[]): void;
+}
+
+class Ball extends Entity {
+  private radius: number;
   private speed: number;
-  private canvas: HTMLCanvasElement;
-  private context: CanvasRenderingContext2D;
 
-  constructor(canvas: HTMLCanvasElement) {
-    this.x = 0;
-    this.y = 0;
+  constructor(x: number, y: number) {
+    super(x, y);
+    this.radius = 10;
     this.speed = 5;
-    this.canvas = canvas;
-    this.context = canvas.getContext("2d");
   }
 
-  draw() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.context.fillRect(this.x, this.y, this.size, this.size);
+  draw(context: CanvasRenderingContext2D) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
   }
 
-  update() {
+  update(canvas: HTMLCanvasElement, entities: Entity[]) {
+    this.x += this.speed;
+    this.y += this.speed;
+
+    if (this.x - this.radius < 0 || this.x + this.radius > canvas.width) {
+      this.speed = -this.speed;
+    }
+    if (this.y - this.radius < 0 || this.y + this.radius > canvas.height) {
+      this.speed = -this.speed;
+    }
+  }
+}
+
+class Player extends Entity {
+  private size: number = 50;
+  private speed: number;
+
+  constructor(x: number, y: number) {
+    super(x, y);
+    this.speed = 5;
+  }
+
+  draw(context: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(this.x, this.y, this.size, this.size);
+  }
+
+  update(canvas: HTMLCanvasElement, entities: Entity[]) {
     if (this.x < 0) {
       this.x = 0;
     }
     if (this.y < 0) {
       this.y = 0;
     }
-    if (this.x + this.size > this.canvas.width) {
-      this.x = this.canvas.width - this.size;
+    if (this.x + this.size > canvas.width) {
+      this.x = canvas.width - this.size;
     }
-    if (this.y + this.size > this.canvas.height) {
-      this.y = this.canvas.height - this.size;
+    if (this.y + this.size > canvas.height) {
+      this.y = canvas.height - this.size;
     }
   }
 
