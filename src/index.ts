a9f7420ebed3d24db4b4522acb914c963969f136
Simon Schödler <simon.schoedler@hotmail.ch>
2024-08-27 00:12:09 +0200

    Adds initial canvas render engine

--- src/index.ts
+++ src/index.ts
@@ -1,125 +1,128 @@
+const TICK_RATE = 60; // ticks per second
+const TICK_DURATION = 1000 / TICK_RATE; // duration of a tick in milliseconds
+const GRID_WIDTH = 1000; // pixels
+const GRID_HEIGHT = 1000; // pixels
+const GRID_SIZE = 10; // pixels per grid cell (width and height)
+
 (() => {
   document.addEventListener("DOMContentLoaded", (_) => {
     const sketch = document.querySelector(".sketch") as HTMLDivElement;
     const canvas = document.createElement("canvas");
-    const context = canvas.getContext("2d");
-
-    canvas.width = 800;
-    canvas.height = 600;
-
     sketch.appendChild(canvas);
 
-    const player = new Player(50, 50);
-    const ball = new Ball(canvas.width / 2, canvas.height / 2);
+    // Create & configure the render engine
+    const engine = new RenderEngine(canvas, GRID_WIDTH, GRID_HEIGHT, GRID_SIZE);
 
-    const entities = [player, ball];
-
-    document.addEventListener("keydown", (event) => {
-      player.handleKeyDown(event);
+    engine.renderTasks.push((data) => {
+      engine.variingFloodFill(data, { r: 200, g: 100, b: 50 }, 30);
+    });
+    engine.renderTasks.push((data) => {
+      for (let x = 0; x < engine.width; x++) {
+        engine.setCell(data, x, engine.height / 2, { r: 0, g: 0, b: 0 });
+      }
     });
 
-    // Game loop using requestAnimationFrame
-    function gameLoop() {
-      entities.forEach((entity) => {
-        entity.update(canvas, entities);
-        entity.draw(context, canvas);
-      });
+    engine.reset();
+
+    // Define the game loop
+    let lastTime = 0;
+    let data: ImageData;
+    function gameLoop(currentTime: number) {
+      const deltaTime = currentTime - lastTime;
+
+      if (!data) {
+        data = engine.render();
+      }
+
+      if (deltaTime > TICK_DURATION) {
+        lastTime = currentTime - (deltaTime % TICK_DURATION);
+        engine.put(data);
+        data = undefined;
+      }
 
       requestAnimationFrame(gameLoop);
     }
 
-    gameLoop();
+    // Start
+    requestAnimationFrame(gameLoop);
   });
 })();
 
-abstract class Entity {
-  protected x: number;
-  protected y: number;
+type Color = { r: number; g: number; b: number; a?: number };
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+class RenderEngine {
+  public readonly width: number;
+  public readonly height: number;
+  private readonly context: CanvasRenderingContext2D;
+  public readonly renderTasks: ((data: ImageData) => void)[] = [];
+
+  constructor(
+    private readonly canvas: HTMLCanvasElement,
+    private readonly canvasWidth: number,
+    private readonly canvasHeight: number,
+    private readonly gridSize: number
+  ) {
+    this.context = this.canvas.getContext("2d", { willReadFrequently: true });
+
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
+
+    this.width = Math.floor(this.canvasWidth / this.gridSize);
+    this.height = Math.floor(this.canvasHeight / this.gridSize);
+
+    this.context.fillStyle = "black";
   }
 
-  abstract draw(
-    context: CanvasRenderingContext2D,
-    canvas: HTMLCanvasElement
-  ): void;
-  abstract update(canvas: HTMLCanvasElement, entities: Entity[]): void;
-}
-
-class Ball extends Entity {
-  private radius: number;
-  private speed: number;
-
-  constructor(x: number, y: number) {
-    super(x, y);
-    this.radius = 10;
-    this.speed = 5;
+  public reset() {
+    this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
   }
 
-  draw(context: CanvasRenderingContext2D) {
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.fill();
+  public put(data: ImageData) {
+    this.context.putImageData(data, 0, 0);
   }
 
-  update(canvas: HTMLCanvasElement, entities: Entity[]) {
-    this.x += this.speed;
-    this.y += this.speed;
+  public render(): ImageData {
+    const data = this.context.getImageData(
+      0,
+      0,
+      this.canvasWidth,
+      this.canvasHeight
+    );
+    this.renderTasks.forEach((task) => task(data));
+    return data;
+  }
 
-    if (this.x - this.radius < 0 || this.x + this.radius > canvas.width) {
-      this.speed = -this.speed;
+  public setCell(data: ImageData, x: number, y: number, color: Color) {
+    // TODO: Possibly optimize this away, not required if the cell indices are within bounds
+    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+      return;
     }
-    if (this.y - this.radius < 0 || this.y + this.radius > canvas.height) {
-      this.speed = -this.speed;
-    }
-  }
-}
 
-class Player extends Entity {
-  private size: number = 50;
-  private speed: number;
+    const px = x * this.gridSize;
+    const py = y * this.gridSize;
 
-  constructor(x: number, y: number) {
-    super(x, y);
-    this.speed = 5;
-  }
-
-  draw(context: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillRect(this.x, this.y, this.size, this.size);
-  }
-
-  update(canvas: HTMLCanvasElement, entities: Entity[]) {
-    if (this.x < 0) {
-      this.x = 0;
-    }
-    if (this.y < 0) {
-      this.y = 0;
-    }
-    if (this.x + this.size > canvas.width) {
-      this.x = canvas.width - this.size;
-    }
-    if (this.y + this.size > canvas.height) {
-      this.y = canvas.height - this.size;
+    for (let i = px; i < px + this.gridSize; i++) {
+      for (let j = py; j < py + this.gridSize; j++) {
+        const index = (j * data.width + i) * 4;
+        data.data[index] = color.r;
+        data.data[index + 1] = color.g;
+        data.data[index + 2] = color.b;
+        data.data[index + 3] = 255;
+      }
     }
   }
 
-  handleKeyDown(event: KeyboardEvent) {
-    switch (event.key) {
-      case "ArrowUp":
-        this.y -= this.speed;
-        break;
-      case "ArrowDown":
-        this.y += this.speed;
-        break;
-      case "ArrowLeft":
-        this.x -= this.speed;
-        break;
-      case "ArrowRight":
-        this.x += this.speed;
-        break;
+  public variingFloodFill(data: ImageData, color: Color, variance: number) {
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        const sign = Math.random() > 0.5 ? 1 : -1;
+        const random = Math.random() * variance * sign;
+        const r = color.r + random;
+        const g = color.g + random;
+        const b = color.b + random;
+        this.setCell(data, x, y, { r, g, b });
+      }
     }
   }
 }
